fff21df77f861f3f0ffe4273f2f4b0e6cf3aa133
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 15:44:40 -0500

    added a file

---
+++ ch03/try_except_code.py
@@ -0,0 +1,10 @@
+# Chapter 3 quiz: For the following code: 
+# What will the value be for istr after this code executes?
+# Answer: -1
+
+astr = 'Hello Bob'
+istr = 0
+try:
+    str = int(astr)
+except:
+    istr = -1
