dfe20df2fd902998b986620f4f5a996810484542
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 15:30:55 -0500

    added a file

---
+++ ch02/ch2_ex_03.py
@@ -0,0 +1,12 @@
+# Exercise 3: write a program to prompt the user for hours and rate per hour to compute gross pay.
+# Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25) 
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+# you should use input to read a string and float() to convert the string to a number.
+# do not worry about error checking or bad user data.
+
+hrs = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+print("pay:", hrs * rate)
+
