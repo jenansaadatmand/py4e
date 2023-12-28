e8ca0b4da1ee38e8b14d93c5134911798ee074ed
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 15:45:07 -0500

    added a file

---
+++ ch03/last_line_to_execute_successfully.py
@@ -0,0 +1,11 @@
+# Ch. 3 quiz: In the following code(numbers added)
+# which will be the last line to execute successfully ?
+# answer: line # 1 in the code 
+
+astr = 'Hello Bob'  # string value
+istr = int(astr)  # int of str will give error
+print('First', istr)
+astr = '123'
+istr = int(astr)
+print('Second', istr)
+
