3e38df0aea808084e0cca5301c8072bc43d7607a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 10:52:16 -0500

    Add files via upload
    A program that computes the percentage of the hour that has elapsed

---
+++ ch01/program_compute_percentage_of_elapsed_time.py
@@ -0,0 +1,7 @@
+# Chapter 2, page 39: 
+# A Program that computes the percentage of the hour that has elapsed
+
+minutes = input('Enter minutes:\n') 
+minutes = int(minutes)
+percentage = (minutes * 100) / 60 # percentage of an hour
+print("Percentage = ", percentage)
