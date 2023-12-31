f3aa8a0869228ce9bb7798a6b577994e0989f981
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:23:48 -0500

    Update ch3_ex_03.py

--- ch03/ch3_ex_03.py
+++ ch03/ch3_ex_03.py
@@ -30,7 +30,7 @@
 
 # If the user enters a value out of range, 
 # Print a suitable error message and exit. For the test, enter a score of 0.85.
-# desired output B, enter 0.8
+# Desired output B, enter 0.8
 
 try:
     grade = float(input("Enter Score: "))
