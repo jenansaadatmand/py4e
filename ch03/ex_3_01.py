ba84e1a68648ee8d37290bb31b6d4989db60f85c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:27:51 -0500

    Update ex_3_01.py

--- ch03/ex_3_01.py
+++ ch03/ex_3_01.py
@@ -1,4 +1,4 @@
-# Exercise 3.1 on autograder:
+# Exercise 3.1 on Atograder:
 # Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
 # Pay the hourly rate for the hours up to 40 
 # And 1.5 times the hourly rate for all hours worked above 40 hours. 
