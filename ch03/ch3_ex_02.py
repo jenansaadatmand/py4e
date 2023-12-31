abb2909d0b3e8fad11b2e1f1d7cde199d3c8fc13
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:22:28 -0500

    Update ch3_ex_02.py

--- ch03/ch3_ex_02.py
+++ ch03/ch3_ex_02.py
@@ -1,7 +1,7 @@
 # Exercise 2: 
 # Rewrite your pay program using try and except 
 # So that your program handles non-numeric input gracefully
-# by printing a message and exiting the program
+# By printing a message and exiting the program
 # the following shows two executions of the program:
 # Enter Hours: 20
 # Enter Rate: nine
