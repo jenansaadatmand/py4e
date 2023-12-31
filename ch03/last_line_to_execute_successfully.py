0eb18cb09d058185681834ecf8bc7a8e5e4df20f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:30:00 -0500

    Update last_line_to_execute_successfully.py

--- ch03/last_line_to_execute_successfully.py
+++ ch03/last_line_to_execute_successfully.py
@@ -1,6 +1,6 @@
 # Chapter 3 quiz: In the following code(numbers added)
 # Which will be the last line to execute successfully?
-# answer: line # 1 in the code 
+# Answer: line # 1 in the code 
 
 astr = 'Hello Bob'  # string value
 istr = int(astr)  # int of str will give error
