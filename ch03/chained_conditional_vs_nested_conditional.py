c27d31cbadd0af35e8f568b56c679474ea9d4bdc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:26:53 -0500

    Update chained_conditional_vs_nested_conditional.py

--- ch03/chained_conditional_vs_nested_conditional.py
+++ ch03/chained_conditional_vs_nested_conditional.py
@@ -35,7 +35,7 @@ if 0 < x:
 
 print() 
 
-# The print statement is excuted only if we make it past both conditionals in this nested conditional  
+# The print statement is executed only if we make it past both conditionals in this nested conditional  
 # We can get same effect if we used the logical boolean and operator
 
 if 0 < x and x < 10:
