81886c4598d2b64880ae6b9496ad1c2114e21624
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:25:28 -0500

    Update ch3_ex_03.py

--- ch03/ch3_ex_03.py
+++ ch03/ch3_ex_03.py
@@ -58,7 +58,7 @@ else:
 # If the score is out of range, print an error. 
 # If the score is between 0.0 and 1.0, 
 # Print a grade using the following table:
-#Score Grade
+# Score Grade
 # >= 0.9 A
 # >= 0.8 B
 # >= 0.7 C
